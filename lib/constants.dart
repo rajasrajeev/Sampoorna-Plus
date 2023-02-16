06c3ec8c55b34ca2f6e503c2c807913389319749
Renjith S <renjithmanamboor123@gmail.com>
2023-02-16 12:17:29 +0530

    primary color change

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 //app color details
-const primaryColor = Color.fromARGB(255, 65, 94, 255);
+const primaryColor = Color.fromARGB(255, 196, 9, 9);
 const backgroundColor = Color(0xFFFFFFFF);
 const cardColor = Color(0xFFE6E6FA);
 
